a60daf0056dc9079402d2e9c45629cf757523dbe
jcowen-Berklee <jcowen@berklee.edu>
2015-11-04 12:46:27 -0500

    added the basic unity project

---
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -0,0 +1,22 @@
+﻿//example provided by http://www.cookingwithunity.com/
+
+using UnityEngine;
+using System.Collections;
+
+public class ApplyForceInRandomDirection : MonoBehaviour
+{
+	public string buttonName = "Fire1";
+	public float forceAmount = 10.0f;
+	public float torqueAmount = 10.0f;
+	public ForceMode forceMode;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown(buttonName))
+		{
+			GetComponent<Rigidbody>().AddForce(Random.onUnitSphere*forceAmount,forceMode);
+			GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
+		}
+	}
+}
---
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/SetMaxAngularVelocity.cs
@@ -0,0 +1,15 @@
+﻿//example provided by http://www.cookingwithunity.com/
+
+using UnityEngine;
+using System.Collections;
+
+public class SetMaxAngularVelocity : MonoBehaviour
+{
+	public float maxAngularVelocity = 7.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Rigidbody>().maxAngularVelocity = maxAngularVelocity;
+	}
+}
