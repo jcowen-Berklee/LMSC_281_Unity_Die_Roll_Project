7005073c3ed8662d0de132bba54725785498db82
antonioespinosah <aespinosa@berklee.edu>
2016-11-17 10:23:18 -0500

    First step: Die automation

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -9,14 +9,16 @@ public class ApplyForceInRandomDirection : MonoBehaviour
 	public float forceAmount = 10.0f;
 	public float torqueAmount = 10.0f;
 	public ForceMode forceMode;
+	public int rolls = 0;
 
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetButtonDown(buttonName))
+		if(rolls<100)
 		{
 			GetComponent<Rigidbody>().AddForce(Random.onUnitSphere*forceAmount,forceMode);
 			GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
+			rolls++;
 		}
 	}
 }
