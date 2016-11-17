42218d393955cbb229a2bcf1309a54079c211d2c
antonioespinosah <aespinosa@berklee.edu>
2016-11-17 11:55:21 -0500

    Write to text

---
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ArrayToText.cs
@@ -0,0 +1,31 @@
+﻿//Antonio Espinosa Holguín
+//November 17th, 2016
+//Based on text to file walkthrough
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ArrayToText : MonoBehaviour {
+
+	string fileToWriteTo;
+	public string stringOfValues;
+	public bool runWriteArray = false;
+
+	// Use this for initialization
+	void Start () {
+		//test
+		stringOfValues = "test text";
+
+		fileToWriteTo = Application.dataPath + "/Resources/Data.txt";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (runWriteArray) {
+			WriteArray ();
+		}
+	}
+	public void WriteArray () {
+		File.AppendAllText (fileToWriteTo, stringOfValues);
+}
+}
